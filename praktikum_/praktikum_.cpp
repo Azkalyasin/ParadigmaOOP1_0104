595de2884134141a2bb72040d47ae1063f66fd04
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-05-20 10:28:38 +0700

    pembuatan classs mahasiswa

--- praktikum_/praktikum_.cpp
+++ praktikum_/praktikum_.cpp
@@ -5,18 +5,7 @@ using namespace std;
 
 
 class mahasiswa {
-private:
-    const int id;
-    string nama;
-    float nilai;
-public:
-    mahasiswa(int pid, string pnama, float pnilai) : id(pid), nama(pnama) {
 
-    }
-
-    mahasiswa() {
-        cout << "ID = " << id << endl;
-    }
 };
 
 int main()
