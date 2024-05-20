99e229bc6037db063fa13f829037b532a646590e
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-05-20 11:02:14 +0700

    project bagian ke 2

---
+++ bagian2/jantung.h
@@ -0,0 +1,18 @@
+
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung {
+public:
+	jantung() {
+		cout << "jantung di hidupkan\n";
+	}
+
+	~jantung() {
+		cout << "jantung di matikan\n";
+	}
+};
+
+#endif
---
+++ bagian2/manusia.h
@@ -0,0 +1,21 @@
+#ifndef MANUSIA_H
+#define MANUSIA_H
+
+using namespace std;
+
+class manusia {
+public:
+	string nama;
+	jantung varjantung;
+
+
+	manusia(string pnama)
+		:nama(pnama) {
+		cout << nama << "hidup\n";
+	}
+	~manusia() {
+		cout << nama << "mati\n";
+	}
+};
+#endif 
+
