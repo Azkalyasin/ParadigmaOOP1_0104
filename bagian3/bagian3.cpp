eb2113ffbccfa3c8d109892c6c35459c146004ed
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-05-20 11:50:36 +0700

    dan memanggil include anak

--- bagian3/bagian3.cpp
+++ bagian3/bagian3.cpp
@@ -9,7 +9,30 @@ using namespace std;
 
 int main()
 {
- 
+    ibu* varibu = new ibu("dini");
+    ibu* varibu2 = new ibu("novi");
+    anak* varanak1 = new anak("tono");
+    anak* varanak2 = new anak("")
+        anak * varanak3 = new anak("")
+
+
+        varibu->tambahanak(varanak1);
+    varibu->tambahanak(varanak2);
+    varibu2->tambahanak(varanak3);
+    varibu2->tambahanak(varanak1);
+
+
+    varibu->cetakanak();
+    varibu2->cetakanak();
+
+    delete varibu;
+    delete varibu2;
+    delete varanak1;
+    delete varanak2;
+    delete varanaK3;
+
+    return 0;
+
 }
 
 
